78cc3de2edeccc859869a115336e5cd171408400
jac.waring <jac.waring@S26-5CG5381986>
2026-02-15 16:01:27 +1100

    added a return to stop the code from running after a player hasnt typed in the correct letters/words and also added a loop and a break so that if you die it stops the loop and you cant keep playing the game

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ int playerLives = 1;
 int playerHealth = 200;
 string landZone1 = "tilted towers";
 string landZone2 = "loot lake";
-Console.WriteLine("Welcome to Fortnite Battle Royale");
+Console.WriteLine("Welcome to Fortnite Battle Royale 1V1");
+
+// how much health you have still
+Console.WriteLine("Health = 200");
 
 
 Console.Write("Where do you want to land? ");
@@ -29,9 +32,70 @@ else
 {
     Console.WriteLine("That is not the correct input");
     Console.WriteLine("Please try again");
+    return;
 }
 
 Console.WriteLine("You see a chest, do you want to open it?");
+Console.Write("yes/no ");
+
+string openChest = "yes";
+string noChest = "no";
+
+string response2 = Console.ReadLine();
+
+if (response2.ToLower() == openChest)
+{
+    Console.WriteLine("You opened the chest and got a blue shotgun");
+}
+else if (response2.ToLower() == noChest)
+{
+    Console.WriteLine("You didnt open the chest and got 200 pumped");
+    playerHealth = 0;
+}
+else
+{
+    Console.WriteLine("That is not the correct input");
+    Console.WriteLine("Please try again");
+    return;
+}
+
+while (true)
+{
+    if (playerHealth == 0)
+    {
+        Console.WriteLine("Health = 0");
+        Console.WriteLine("Game over, you died");
+        playerLives = 0;
+        break;
+    }
+    else
+    {
+        string shoot = "yes";
+        string dontShoot = "no";
+
+        Console.WriteLine("You see a player to your right, do you shoot him");
+        Console.Write("yes/no ");
+        string response3 = Console.ReadLine();
+
+        if (response3.ToLower() == shoot)
+        {
+            Console.WriteLine("You killed him and lived");
+            Console.WriteLine("Health = 200");
+        }
+        else if (response3.ToLower() == dontShoot)
+        {
+            Console.WriteLine("you didnt shoot him and got 200 pumped");
+            playerHealth = 0;
+        }
+        else
+        {
+            Console.WriteLine("That is not the correct input");
+            Console.WriteLine("Please try again");
+            return;
+        }
+    }
+}
+
 
 
 
